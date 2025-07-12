dd8d8794a2ccf97a28d1ebcc7c7dcf9500a2b04c
syauraa <syifarahma336@gmail.com>
2025-07-12 08:45:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pandas as pd
 
-st.write("Hello, *World!* :sunglasses:")
+st.write(1234)
+st.write(
+    pd.DataFrame(
+        {
+            "first column": [1, 2, 3, 4],
+            "second column": [10, 20, 30, 40],
+        }
+    )
+)
